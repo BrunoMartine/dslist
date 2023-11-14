dbf194956259bfa26af8cd735745baf8e8711ee2
BrunoMartine <bruno.r.martine@gmail.com>
2023-11-14 14:49:27 -0300

    Project created

---
+++ src/test/java/com/brunomartine/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomartine.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
